a65fe583f9f6baa1a8fc202e5f6844d520eb0cc0
Shubham Kumar <ishubhamjnv@gmail.com>
2023-10-02 22:50:19 +0530

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,13 @@
+const myName = "Lakshay";
+const id = 4;
+const isLoggedIn = true;
+const city = null;
+let age;
+
+console.table([
+  typeof myName,
+  typeof id,
+  typeof isLoggedIn,
+  typeof city,
+  typeof age,
+]);
