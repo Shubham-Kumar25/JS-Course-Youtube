e8ecceb432b2aee9a007598f78def7b133259646
Shubham Kumar <ishubhamjnv@gmail.com>
2023-10-02 22:12:56 +0530

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountID = 676;
+let accountName = "Lakshay";
+var accountEmail = "laxsharma@gmail.com";
+let accountCity;
+accountName = "shubham";
+// accountID = 6;
+
+console.table([accountID, accountName, accountEmail, accountCity]);
