c240d33b7d25c02e86bd9eebeebcfd537cf9dd41
Shubham Kumar <ishubhamjnv@gmail.com>
2023-10-03 23:44:42 +0530

    stack and heap memory

---
+++ 01_basics/05_stackHeap.js
@@ -0,0 +1,16 @@
+const name = "Lakshay";
+let myName = name;
+myName = "shubham";
+
+// console.log(name);
+// console.log(myName);
+
+const userOne = {
+  name: "Lakshay",
+  age: 23,
+};
+
+let userTwo = userOne;
+userTwo.age = 24;
+console.log(userOne);
+console.log(userTwo);
