099b6c3a28129f91f2f8ae051c274a2f960c0f2d
Shubham Kumar <ishubhamjnv@gmail.com>
2023-10-03 23:15:01 +0530

    datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,31 @@
+const str1 = "lakshay";
+const score = 5;
+const marks = 8.8;
+const isLoggedIn = true;
+const temp = null;
+let city;
+const bigInt = 79529379375913759319531n;
+const id = Symbol("123");
+
+// console.log(typeof str1);
+// console.log(typeof score);
+// console.log(typeof marks);
+// console.log(typeof isLoggedIn);
+// console.log(typeof temp);
+// console.log(typeof city);
+// console.log(typeof bigInt);
+// console.log(typeof id);
+
+const sweets = ["gud", "shakkar", "cheeni"];
+const myDetail = {
+  name: "Lakshay",
+  age: 24,
+  college: "Meerut College",
+};
+const myFun = function () {
+  return "Hello World";
+};
+
+console.log(typeof sweets);
+console.log(typeof myDetail);
+console.log(typeof myFun);
