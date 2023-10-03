1d319c1b73f657861c2f58ce895c1c2d27058134
Shubham Kumar <ishubhamjnv@gmail.com>
2023-10-03 13:31:10 +0530

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,11 @@
+const score = null;
+const numberScore = Number(score);
+const stringScore = String(score);
+const booleanScore = Boolean(score);
+const objectScore = Object(score);
+
+console.log(typeof score, score);
+console.log(typeof numberScore, numberScore);
+console.log(typeof stringScore, stringScore);
+console.log(typeof booleanScore, booleanScore);
+console.log(typeof objectScore, objectScore);
