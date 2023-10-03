ee33ab6846d10b8170a48ca794b27403205d660b
Shubham Kumar <ishubhamjnv@gmail.com>
2023-10-03 14:21:27 +0530

    some more conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -4,8 +4,29 @@ const stringScore = String(score);
 const booleanScore = Boolean(score);
 const objectScore = Object(score);
 
-console.log(typeof score, score);
-console.log(typeof numberScore, numberScore);
-console.log(typeof stringScore, stringScore);
-console.log(typeof booleanScore, booleanScore);
-console.log(typeof objectScore, objectScore);
+// console.log(typeof score, score);
+// console.log(typeof numberScore, numberScore);
+// console.log(typeof stringScore, stringScore);
+// console.log(typeof booleanScore, booleanScore);
+// console.log(typeof objectScore, objectScore);
+
+// const str1 = "Hello";
+// const str2 = " Lakshay";
+
+// const str3 = str1 + str2;
+
+// console.log(str3, typeof str3);
+
+// console.log(typeof ("1" + 2 + 3));
+// console.log(typeof (1 + 2 + "3"));
+
+let value = 3;
+// let negValue = -value;
+// console.log(negValue);
+
+let counter = 100;
+console.log(++counter);
+console.log(counter++);
+
+console.log(counter--);
+console.log(--counter);
