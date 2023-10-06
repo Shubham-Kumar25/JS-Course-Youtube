defeb7ccff589e5a1a1c2324c11896f014078936
Shubham Kumar <ishubhamjnv@gmail.com>
2023-10-06 12:07:38 +0530

    array basics

---
+++ 02_basics/01_array.js
@@ -0,0 +1,43 @@
+const myArray = [1, 2, 3, 4, "hello", true];
+// console.log(myArray[4]);
+
+const arr1 = new Array(1, 2, 3, 4);
+console.log(typeof arr1, arr1);
+
+arr1.push(5);
+console.log(arr1);
+
+arr1.pop();
+console.log(arr1);
+
+arr1.shift();
+console.log(arr1);
+
+arr1.unshift(3);
+console.log(arr1);
+
+arr1.shift();
+console.log(arr1);
+
+console.log(arr1.includes(4));
+console.log(arr1.indexOf(3));
+
+const arr2 = new Array(1, 2, 3, 4);
+const arr3 = new Array(1, 2, 3, 4);
+
+console.log(arr2);
+console.log(arr3);
+
+console.log(arr2.slice(1, 3));
+arr2.push(5);
+console.log(arr2);
+
+console.log(arr3.splice(1, 3));
+console.log(arr3);
+arr3.push(5);
+console.log(arr3);
+
+const strArr = ["neem", "amrud", "aam"];
+console.log(typeof strArr, strArr);
+
+console.log(strArr.join(), typeof strArr.join("-"));
