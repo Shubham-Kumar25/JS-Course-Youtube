a43d7f76785f557ce7de843be23f0e70e718bd4c
Shubham Kumar <ishubhamjnv@gmail.com>
2023-11-10 15:17:26 +0530

    Arrow Function

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,30 @@
+const user = {
+  username: "Shubham",
+  price: 999,
+  welcomeMessage: function () {
+    // console.log(`Welcome, ${this.username}`);
+    // console.log(this);
+  },
+};
+
+// console.log(this);
+
+user.welcomeMessage();
+user.username = "Lakshay";
+user.welcomeMessage();
+
+// function chai() {
+//   let username = "Shubham";
+//   console.log(this.username);
+// }
+// console.log(this);
+// chai();
+
+const chai = () => {
+  let username = "shubham";
+  //   console.log(this);
+};
+chai();
+
+const addTwo = (num1, num2) => num1 + num2;
+console.log(addTwo(4, 6));
