9457ae9dbee11400dce6843cc726be2097fe4538
Shubham Kumar <ishubhamjnv@gmail.com>
2023-11-10 13:41:56 +0530

    functions basics

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,22 @@
+// function myName() {
+//      console.log("shubham");
+// }
+// myName();
+// myName;
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+let result = add(4, 8);
+// console.log(result);
+
+function loginUserMessage(userName = "Lxy") {
+  if (!userName) {
+    // console.log("please enter valid userName");
+    return;
+  } else {
+    // console.log("Hello", userName);
+  }
+}
+
+// loginUserMessage();
