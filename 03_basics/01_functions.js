f5737ec136daf8f58905eec18b41c8e8dd17002e
Shubham Kumar <ishubhamjnv@gmail.com>
2023-11-10 13:49:25 +0530

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -20,3 +20,26 @@ function loginUserMessage(userName = "Lxy") {
 }
 
 // loginUserMessage();
+
+function calculateCartPrice(...num1) {
+  return num1;
+}
+// console.log(calculateCartPrice(100, 200, 300, 400));
+
+const user = {
+  id: 1,
+  name: "shubham",
+  age: 23,
+};
+
+function handleObject(anyObject) {
+  //   console.log(`Welcome, ${anyObject.name} `);
+}
+handleObject(user);
+
+const arr = [20, 30, 40];
+
+function handleArr(getArr) {
+  console.log(getArr[1]);
+}
+handleArr(arr);
