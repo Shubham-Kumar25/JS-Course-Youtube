f16c7b073c6c8acd19c57c7d2ae8cc4ebacb6489
Shubham Kumar <ishubhamjnv@gmail.com>
2023-11-11 13:50:39 +0530

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,16 @@
+const month = "Apr";
+
+switch (month) {
+  case "Jan":
+    console.log("31 days in this month");
+    break;
+  case "Feb":
+    console.log("28 or 29 days in this month");
+    break;
+  case "Mar":
+    console.log("31 days in this month");
+    break;
+  default:
+    console.log("30 or 31 days in this month");
+    break;
+}
