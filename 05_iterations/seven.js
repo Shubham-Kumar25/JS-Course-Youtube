5bcacd0393fd3b0bc3b6610541a440213984a41c
Shubham Kumar <ishubhamjnv@gmail.com>
2023-11-17 00:00:39 +0530

    map()

---
+++ 05_iterations/seven.js
@@ -0,0 +1,12 @@
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const newNums = myNums.map((num) => {
+  return num + 10;
+});
+// console.log(newNums);
+
+const chainingNums = newNums
+  .map((num) => num * 10)
+  .map((num) => num + 5)
+  .filter((num) => num >= 130);
+console.log(chainingNums);
