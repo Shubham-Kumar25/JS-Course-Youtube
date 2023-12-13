25afd7731132df7ec17a303677bfcb0087d62c81
Shubham Kumar <ishubhamjnv@gmail.com>
2023-12-13 13:34:32 +0530

    advance array

---
+++ 12_advance/array.js
@@ -0,0 +1,7 @@
+const arr1 = [1, 2, 3, 4, 5]; //PACKED_SMI
+
+const arr2 = [1, 2, 3, 4, 5.3]; //PACKED_DOUBLE
+
+const arr3 = [1, 2, 3, "a", 6]; //PACKED
+
+const arr4 = [1, 2, 3, , , , , 6, 7]; //HOLEY
